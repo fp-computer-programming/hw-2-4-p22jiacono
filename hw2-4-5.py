9a1c1cadadf046912f87c7d3a85db7314dc530be
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-09-26 17:19:35 -0400

    hw2-4-5

---
+++ hw2-4-5.py
@@ -0,0 +1,12 @@
+#Author: JTI 9/23/21
+
+a=1 
+b=3 
+c=-4
+
+
+first_solution=(-b + (b ** 2) - (4 * a * -c) / (2 * a))
+
+second_solution=(-b - (b ** 2) - (4 * a * -c) / (2 * a))
+
+print(first_solution,second_solution)
